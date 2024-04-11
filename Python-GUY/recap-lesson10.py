994feeb537ae6ba49d2cf318c8daf94e9d1b9d3c
J.V.G.I.E Prasad <126505521+IEPrasad@users.noreply.github.com>
2024-04-11 21:18:36 +0530

    Create recap-lesson10.py

---
+++ Python-GUY/recap-lesson10.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+root.title("Practice-01")
+root.geometry("300x300")
+root.resizable(False, False)
+
+btn = ttk.Button(root, text="Click me")
+btn.pack()
+
+entry = ttk.Entry(root)
+entry.pack()
+
+entry.bind('<FocusIn>', lambda event:print("Entry field selected"))
+entry.bind("<FocusOut>", lambda event:print("Event field deselected"))
+
+root.mainloop()
+
+
+
+# this is about the lesson 10 and this is the same code in lesson 10 
+# this code for the practice again about the python GUY
